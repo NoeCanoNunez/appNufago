0c6d6bc4184ec52981b746f8c2ce8eb2fe55f762
Your NameNoeCanoNunez <47346373@continental.edu.pe>
2024-08-24 21:25:06 -0500

    Mejora de UIX

--- src/components/UIX.js
+++ src/components/UIX.js
@@ -1,36 +1,26 @@
 import React, { useState } from "react";
-import VentaForm from "./VentaForm";
-import {
-  InboxArrowDownIcon,
-  ShoppingBagIcon,
-  BookOpenIcon,
-  BoltIcon,
-  UserGroupIcon,
-  ArrowUpOnSquareIcon,
-  ArchiveBoxIcon
-
-} from "@heroicons/react/24/outline";
+import { InboxArrowDownIcon, ShoppingBagIcon, BookOpenIcon, BoltIcon, UserGroupIcon, ArrowUpOnSquareIcon, ArchiveBoxIcon } from "@heroicons/react/24/outline";
 import Options from "./UIX/Options";
+import VentaForm from "./VentaForm";
 import UsersForm from "./UsersForm";
-
-
 import IngresarAlmacen from "./IngresarAlmacen";
 import MostrarVentas from "./MostrarVentas";
-import MostrarVentaVendedor from "./MostrarVentaVendedor"
+import MostrarVentaVendedor from "./MostrarVentaVendedor";
 import RepartirAlmacen from "./RepartirAlmacen";
 import MegaBuscador from "./MegaBuscador";
 import InventariosTienda from "./InventariosTienda";
 
 const WindowsUIComponent = ({ vendedor }) => {
   const [activeTab, setActiveTab] = useState("Ventas");
-  const [showSidebar, setShowSidebar] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
   const handleTabChange = (tab) => {
     setActiveTab(tab);
-    setShowSidebar(false);
   };
 
-  console.log(vendedor)
+  const toggleSidebar = () => {
+    setIsCollapsed(!isCollapsed);
+  };
 
   const primaryColor = "bg-gradient-to-tl from-orange-500 to-rose-600";
   const secondaryColor =
@@ -41,138 +31,89 @@ const WindowsUIComponent = ({ vendedor }) => {
     "hover:bg-gradient-to-r hover:from-amber-400 hover:to-pink-300/70";
 
   return (
-    <div className="flex flex-col h-screen bg-gray-200 lg:flex-row">
-      {/*Version para Moviles*/}
-      <div className="lg:hidden flex justify-start shadow mb-0.5 p-2 rounded-b-md bg-gradient-to-tl from-orange-500 to-rose-600">
-        <button
-          onClick={() => setShowSidebar(!showSidebar)}
-          className="mx-2 text-gray-500 hover:text-blue-700"
-        >
-          <svg
-            className="w-8 h-8"
-            color="white"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </button>
-        <div className="flex items-center ml-5">
-          <img
-            src={`https://ui-avatars.com/api/?name=${
-              vendedor.nombres + " " + vendedor.apPaterno
-            }&background=random`}
-            alt={vendedor.nombres}
-            className="w-10 h-10 rounded-full mr-4"
+    <div className="flex h-screen bg-gray-200">
+      {/* Sidebar */}
+      <div className={`transition-all duration-300 ease-in-out ${isCollapsed ? 'w-14' : 'w-64'} ${primaryColor} shadow-lg`}>
+        {/* Logo y botón de colapso */}
+        <div className="flex items-center justify-between p-4">
+          <button onClick={toggleSidebar} className="focus:outline-none">
+            <svg className="w-6 h-6 text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d={isCollapsed ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"} />
+            </svg>
+          </button>
+        </div>
+
+        {/* Opciones de la barra de herramientas */}
+        <div className="space-y-2">
+          <Options
+            activeTab={activeTab}
+            handleTabChange={handleTabChange}
+            tabName="Inventarioas"
+            Icon={InboxArrowDownIcon}
+            isCollapsed={isCollapsed}
+            tertiaryColor={tertiaryColor}
+          secondaryColor={secondaryColor}
+          cuaternaryColor={cuaternaryColor}
           />
-          <div className="flex flex-col justify-between items-start mb-2">
-            <div className="text-white font-semibold">{vendedor.nombres}</div>
-            <div className="text-white">{vendedor.rol}</div>
-          </div>
-        </div>
-      </div>
-
-      <div
-        className={`lg:block ${primaryColor} p-4 text-white lg:w-64  ${
-          showSidebar
-            ? "w-full translate-x-0"
-            : "-translate-x-full lg:translate-x-0 lg:static fixed flex flex-wrap gap-0.5"
-        }`}
-      >
-        {/*Version para Escritorio*/}
-        <div className="hidden lg:flex justify-start mb-6 px-2 py-3 rounded-b-md bg-gradient-to-tl from-blue-500 to-green-600 -mx-4 -mt-4 shadow-lg shadow-black/30 ">
-          <div className="flex items-center ml-5 ">
-            <img
-              src={`https://ui-avatars.com/api/?name=${
-                vendedor.nombres + " " + vendedor.apPaterno
-              }&background=random`}
-              alt={vendedor.nombres}
-              className="w-10 h-10 rounded-full mr-4 -ml-2"
+          <Options
+            activeTab={activeTab}
+            handleTabChange={handleTabChange}
+            tabName="RepartirInventario"
+            Icon={ArrowUpOnSquareIcon}
+            isCollapsed={isCollapsed}
+            tertiaryColor={tertiaryColor}
+          secondaryColor={secondaryColor}
+          cuaternaryColor={cuaternaryColor}
+          />
+          <Options
+            activeTab={activeTab}
+            handleTabChange={handleTabChange}
+            tabName="MegaBuscador"
+            Icon={BoltIcon}
+            isCollapsed={isCollapsed}
+            tertiaryColor={tertiaryColor}
+          secondaryColor={secondaryColor}
+          cuaternaryColor={cuaternaryColor}
+          />
+          <Options
+            activeTab={activeTab}
+            handleTabChange={handleTabChange}
+            tabName="InventarioTienda"
+            Icon={ArchiveBoxIcon}
+            isCollapsed={isCollapsed}
+            tertiaryColor={tertiaryColor}
+          secondaryColor={secondaryColor}
+          cuaternaryColor={cuaternaryColor}
+          />
+          <Options
+            activeTab={activeTab}
+            handleTabChange={handleTabChange}
+            tabName="ListarVentas"
+            Icon={BookOpenIcon}
+            isCollapsed={isCollapsed}
+            tertiaryColor={tertiaryColor}
+          secondaryColor={secondaryColor}
+          cuaternaryColor={cuaternaryColor}
+          />
+          {vendedor.rol === "ADMINISTRATIVO" && (
+            <Options
+              activeTab={activeTab}
+              handleTabChange={handleTabChange}
+              tabName="Usuarios"
+              Icon={UserGroupIcon}
+              isCollapsed={isCollapsed}
+              tertiaryColor={tertiaryColor}
+          secondaryColor={secondaryColor}
+          cuaternaryColor={cuaternaryColor}
             />
-            <div className="flex flex-col justify-between items-start mb-2">
-              <div className="text-white font-semibold">{vendedor.nombres}</div>
-              <div className="text-white">{vendedor.rol}</div>
-            </div>
-          </div>
+          )}
         </div>
-
-        {vendedor.rol === "ADMINISTRATIVO" && <Options
-          activeTab={activeTab}
-          handleTabChange={handleTabChange}
-          tabName="Inventarios"
-          Icon={InboxArrowDownIcon}
-          tertiaryColor={tertiaryColor}
-          secondaryColor={secondaryColor}
-          cuaternaryColor={cuaternaryColor}
-        />}
-        {vendedor.rol === "ADMINISTRATIVO" && <Options
-          activeTab={activeTab}
-          handleTabChange={handleTabChange}
-          tabName="RepartirInventario"
-          Icon={ArrowUpOnSquareIcon}
-          tertiaryColor={tertiaryColor}
-          secondaryColor={secondaryColor}
-          cuaternaryColor={cuaternaryColor}
-        />}
-        {vendedor.rol === "ADMINISTRATIVO" && <Options
-          activeTab={activeTab}
-          handleTabChange={handleTabChange}
-          tabName="MegaBuscador"
-          Icon={BoltIcon}
-          tertiaryColor={tertiaryColor}
-          secondaryColor={secondaryColor}
-          cuaternaryColor={cuaternaryColor}
-        />}
-        <Options
-          activeTab={activeTab}
-          handleTabChange={handleTabChange}
-          tabName="InventarioTienda"
-          Icon={ArchiveBoxIcon}
-          tertiaryColor={tertiaryColor}
-          secondaryColor={secondaryColor}
-          cuaternaryColor={cuaternaryColor}
-        />
-        <Options
-          activeTab={activeTab}
-          handleTabChange={handleTabChange}
-          tabName="ListarVentas"
-          Icon={BookOpenIcon}
-          tertiaryColor={tertiaryColor}
-          secondaryColor={secondaryColor}
-          cuaternaryColor={cuaternaryColor}
-        />
-        <Options
-          activeTab={activeTab}
-          handleTabChange={handleTabChange}
-          tabName="RegistrarVentas"
-          Icon={ShoppingBagIcon}
-          tertiaryColor={tertiaryColor}
-          secondaryColor={secondaryColor}
-          cuaternaryColor={cuaternaryColor}
-        />
-        {vendedor.rol === "ADMINISTRATIVO" && <Options
-          activeTab={activeTab}
-          handleTabChange={handleTabChange}
-          tabName="Usuarios"
-          Icon={UserGroupIcon}
-          tertiaryColor={tertiaryColor}
-          secondaryColor={secondaryColor}
-          cuaternaryColor={cuaternaryColor}
-        />}
       </div>
 
       {/* Contenido principal */}
-      <div className="flex-1 sm:p-4 p-5 -ml-2.5 sm:ml-0 overflow-y-scroll ">
-        {/* Barra de navegación */}
-        {activeTab === "RepartirInventario" && <RepartirAlmacen />}          
-        {activeTab === "MegaBuscador" && <MegaBuscador/>}
+      <div className="flex-1 p-4 overflow-y-auto">
+        {activeTab === "RepartirInventario" && <RepartirAlmacen />}
+        {activeTab === "MegaBuscador" && <MegaBuscador />}
         {activeTab === "Inventarios" && <IngresarAlmacen />}
         {activeTab === "InventarioTienda" && <InventariosTienda />}
         {activeTab === "RegistrarVentas" && <VentaForm vendedor={vendedor} />}
--- src/components/UIX/Options.js
+++ src/components/UIX/Options.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
-const Options = ({ activeTab, handleTabChange, tabName, Icon, tertiaryColor, secondaryColor, cuaternaryColor }) => {
+const Options = ({ activeTab, handleTabChange, tabName, Icon, isCollapsed, tertiaryColor, secondaryColor, cuaternaryColor }) => {
   return (
     <div
-      className={` shadow-purple-900/40 shadow-md flex items-center px-4 py-2 rounded-md cursor-pointer mb-2 ${
+      className={`shadow-purple-900/40 shadow-md flex items-center mx-2 py-2 rounded-md cursor-pointer mb-2 transition-all duration-300 ${
         activeTab === tabName
           ? `${tertiaryColor}`
           : `${secondaryColor} text-white ${cuaternaryColor} hover:text-white`
       }`}
       onClick={() => handleTabChange(tabName)}
     >
-      <Icon className="w-6 h-6 mr-2" />
-      {tabName}
+      {/* Ícono de la opción */}
+      <Icon className={`w-6 h-6 ${isCollapsed ? "mx-auto" : "mr-2 text-white"}`} />
+      
+      {/* Nombre de la opción (se muestra solo cuando la barra no está colapsada) */}
+      {!isCollapsed && <span className="ml-2">{tabName}</span>}
     </div>
   );
 };
